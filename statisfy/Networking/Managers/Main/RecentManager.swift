36db7cf363ec50f73fb714488adbf32e66c51e68
Avesta Barzegar <Avesta.Barzegar@gmail.com>
2021-03-30 22:57:34 -0400

    new models and endpoint for recent track view has been initialized

---
+++ statisfy/Networking/Managers/Main/RecentManager.swift
@@ -0,0 +1,16 @@
+//
+//  RecentManager.swift
+//  statisfy
+//
+//  Created by Avesta Barzegar on 2021-03-30.
+//
+
+import Foundation
+
+struct RecentManager {
+    
+    static let shared = RecentManager()
+    
+    var recentTrackInfo: [RecentTrackInfo] = []
+        
+}
