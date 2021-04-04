2d50464b66df28fdc5e904d0752282bdd3995386
Avesta Barzegar <Avesta.Barzegar@gmail.com>
2021-04-03 22:10:53 -0400

    Parameter encoder and network errors

--- statisfy/NetworkingTemp/Encoding/ParameterEncoding.swift
+++ statisfy/NetworkingTemp/Encoding/ParameterEncoding.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+public typealias Parameters = [String: Any]
+
+public enum NetworkError: String, Error {
+    case parametersNil = "Parameters were nil."
+    case encodingFailed = "Parameter encoding failed."
+    case missingURL = "URL is nil"
+}
+
+public protocol ParameterEncoder {
+    
+    static func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws
+    
+}
