d74305d3ec941d72aa041b97026b318077d40a40
Avesta Barzegar <Avesta.Barzegar@gmail.com>
2021-04-03 22:11:48 -0400

    Network Router Protocol definition

--- statisfy/NetworkingTemp/Service/NetworkRouter.swift
+++ statisfy/NetworkingTemp/Service/NetworkRouter.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+public typealias NetworkRouterCompletion = (_ data: Data?, _ response: URLResponse?, _ error: Error?) -> Void
+
+protocol NetworkRouter: AnyObject {
+    associatedtype EndPoint: EndPointType
+    func request(_ route: EndPoint, completion: @escaping NetworkRouterCompletion)
+    func cancel()
+}
