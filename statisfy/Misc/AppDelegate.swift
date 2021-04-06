f6a14f7ba6818c4388aa74d250459c24c625ab54
Avesta Barzegar <Avesta.Barzegar@gmail.com>
2021-04-06 17:19:48 -0400

    appDelegate cleanup

--- statisfy/Misc/AppDelegate.swift
+++ statisfy/Misc/AppDelegate.swift
@@ -15,12 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        setRootVc(window: window)
-        window.makeKeyAndVisible()
-        window.overrideUserInterfaceStyle = .dark
-        self.window = window
-        
         return true
     }
 
