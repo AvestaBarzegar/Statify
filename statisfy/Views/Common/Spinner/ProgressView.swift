6087d00860096993d841cc0baa6a2a7632654307
Avesta Barzegar <Avesta.Barzegar@gmail.com>
2021-04-05 22:04:43 -0400

    incorporated ProgressShapeLayer into ProgressView

--- statisfy/Views/Common/Spinner/ProgressView.swift
+++ statisfy/Views/Common/Spinner/ProgressView.swift
@@ -8,15 +8,28 @@
 import Foundation
 import UIKit
 
+import UIKit
+
 class ProgressView: UIView {
     
-    // MARK: - Initialization
-    override init(frame: CGRect) {
+    // MARK: - Initializers
+    init(frame: CGRect,
+         colors: [UIColor],
+         lineWidth: CGFloat
+    ) {
+        self.colors = colors
+        self.lineWidth = lineWidth
+        
         super.init(frame: frame)
         
         self.backgroundColor = .clear
     }
-
+    
+    // Convenience initializers without specification of frame
+    convenience init(colors: [UIColor], lineWidth: CGFloat) {
+        self.init(frame: .zero, colors: colors, lineWidth: lineWidth)
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) is not supported")
     }
@@ -25,5 +38,25 @@ class ProgressView: UIView {
         super.layoutSubviews()
         
         self.layer.cornerRadius = self.frame.width / 2
+        
+        let path = UIBezierPath(ovalIn:
+            CGRect(
+                x: 0,
+                y: 0,
+                width: self.bounds.width,
+                height: self.bounds.width
+            )
+        )
+
+        shapeLayer.path = path.cgPath
     }
+    
+    // MARK: - Animation properties
+    
+    let colors: [UIColor]
+    let lineWidth: CGFloat
+    
+    private lazy var shapeLayer: ProgressShapeLayer = {
+        return ProgressShapeLayer(lineWidth: lineWidth)
+    }()
 }
