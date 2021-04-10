fe874d31e65fc655187f38d17ab39007b93f48f7
Avesta Barzegar <Avesta.Barzegar@gmail.com>
2021-04-09 22:57:20 -0400

    created new custom component files for settings page

---
+++ statisfy/Views/Settings/AccountCardView.swift
@@ -0,0 +1,20 @@
+//
+//  AccountCardView.swift
+//  statisfy
+//
+//  Created by Avesta Barzegar on 2021-04-09.
+//
+
+import UIKit
+
+class AccountCardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
