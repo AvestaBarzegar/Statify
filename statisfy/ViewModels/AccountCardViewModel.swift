6258675550d8defdb6c04ae7a0e89b00d89fcd85
Avesta Barzegar <Avesta.Barzegar@gmail.com>
2021-04-09 23:57:16 -0400

    view model for accountCard

---
+++ statisfy/ViewModels/AccountCardViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  AccountCardViewModel.swift
+//  statisfy
+//
+//  Created by Avesta Barzegar on 2021-04-09.
+//
+
+import Foundation
+
+struct AccountCardViewModel {
+    
+    let email: String?
+    let name: String?
+    let followerCount: Int?
+    let imageURL: String?
+}
