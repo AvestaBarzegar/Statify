82e524f2b65948778cb3283167376cf08ae0dbbd
Avesta Barzegar <Avesta.Barzegar@gmail.com>
2021-04-10 00:22:29 -0400

    configured network requests for account info

--- statisfy/NetworkingNew/EndPoint/UserEndpoint.swift
+++ statisfy/NetworkingNew/EndPoint/UserEndpoint.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+
+public enum UserAPI {
+    case user
+}
+
+extension UserAPI: EndPointType {
+    
+    var baseURL: URL {
+        let baseURL = "https://api.spotify.com"
+        let url = URL(string: baseURL)
+        return url!
+    }
+    
+    var path: String {
+        switch self {
+        case .user:
+            return "/v1/me"
+        }
+    }
+    
+    var httpMethod: HTTPMethod {
+        switch self {
+        case .user:
+            return .get
+        }
+    }
+    
+    var task: HTTPTask {
+        switch self {
+        case .user:
+            return .requestParametersAndHeaders(bodyParameters: nil,
+                                            urlParameters: nil,
+                                            additionalHeaders: headers)
+        }
+    }
+    
+    var headers: HTTPHeaders? {
+        switch self {
+        case .user:
+            let headers: HTTPHeaders = [
+                "Accept": "application/json",
+                "Authorization": "Bearer \(AuthManager.shared.accessToken ?? "")"
+            ]
+            return headers
+        }
+    }
+    
+    
+}
--- statisfy/NetworkingNew/Manager/UserManager.swift
+++ statisfy/NetworkingNew/Manager/UserManager.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+
+struct UserManager {
+    
+    private let router = Router<UserAPI>()
+    
+    func handleNetworkResponse(_ response: HTTPURLResponse) -> Result<String> {
+        
+        switch response.statusCode {
+        case 200...299:
+            return .success
+        case 401...500:
+            return .failure(NetworkResponse.authError.rawValue)
+        case 501...599:
+            return .failure(NetworkResponse.badRequest.rawValue)
+        case 600:
+            return .failure(NetworkResponse.outdated.rawValue)
+        default:
+            return .failure(NetworkResponse.failed.rawValue)
+        }
+    }
+    
+    func getAccountInfo(completion: @escaping(_ items: AccountCardViewModel?, _ error: String?) -> Void) {
+        router.request(.user) { data, response, error in
+            if error != nil {
+                completion(nil, "Please check your network connection.")
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                let result = self.handleNetworkResponse(response)
+                switch result {
+                case .success:
+                    guard let responseData = data else {
+                        completion(nil, NetworkResponse.noData.rawValue)
+                        return
+                    }
+                    do {
+                        let decoder = JSONDecoder()
+                        let account = try decoder.decode(AccountInfoItemModel.self, from: responseData)
+                        let accountViewModel = AccountCardViewModel(cardModel: account)
+                        completion(accountViewModel, nil)
+
+                    } catch {
+                        completion(nil, NetworkResponse.unableToDecode.rawValue)
+                    }
+                case .failure(let networkFailureError):
+                    completion(nil, networkFailureError)
+                }
+            }
+        }
+    }
+    
+}
