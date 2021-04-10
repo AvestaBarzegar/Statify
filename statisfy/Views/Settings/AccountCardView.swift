b6eab1bea6228c0bdb21d437fd353f62ff3c4866
Avesta Barzegar <Avesta.Barzegar@gmail.com>
2021-04-09 22:58:57 -0400

    created layout for accountInformationCard view

--- statisfy/Views/Settings/AccountCardView.swift
+++ statisfy/Views/Settings/AccountCardView.swift
@@ -8,13 +8,89 @@
 import UIKit
 
 class AccountCardView: UIView {
+    
+    // Spotify image - left: from top to bottom: email, name, follower count
+    
+    // MARK: - Init Views
+    
+    private lazy var profileImageView: DownloadedImageView = {
+        let imageView = DownloadedImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = UIColor.spotifyGray
+        imageView.layer.cornerRadius = Constants.cornerRadius.rawValue
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private lazy var emailLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.subHeaderFont
+        label.textColor = UIColor.spotifyWhite
+        label.numberOfLines = 1
+        label.lineBreakMode = .byTruncatingTail
+        label.text = "THIS IS AN EMAIL LABEL"
+        return label
+    }()
+    
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.bodyFontBolded
+        label.textColor = UIColor.spotifyWhite
+        label.numberOfLines = 1
+        label.lineBreakMode = .byTruncatingTail
+        label.text = "THIS IS A NAME LABEL"
+        return label
+    }()
+    
+    private lazy var followerCountLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.bodyFontBolded
+        label.textColor = UIColor.spotifyGreen
+        label.numberOfLines = 1
+        label.lineBreakMode = .byTruncatingTail
+        label.text = "THIS IS A FOLLOWER COUNT LABEL"
+        return label
+    }()
+    
+    
+    // MARK: - Initializer
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        sharedLayout()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: - Layout Views
+    
+    private func sharedLayout() {
+        self.layer.cornerRadius = Constants.cornerRadius.rawValue
+        self.addSubview(profileImageView)
+        self.addSubview(emailLabel)
+        self.addSubview(nameLabel)
+        self.addSubview(followerCountLabel)
+        
+        NSLayoutConstraint.activate([
+            profileImageView.heightAnchor.constraint(equalToConstant: 48.0),
+            profileImageView.widthAnchor.constraint(equalTo: profileImageView.heightAnchor),
+            profileImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 8.0),
+            profileImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            
+            emailLabel.leftAnchor.constraint(equalTo: profileImageView.rightAnchor, constant: 8),
+            emailLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 8.0),
+            
+            nameLabel.leftAnchor.constraint(equalTo: profileImageView.rightAnchor, constant: 8),
+            nameLabel.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 8.0),
+            
+            followerCountLabel.leftAnchor.constraint(equalTo: profileImageView.rightAnchor, constant: 8),
+            followerCountLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8.0),
+        ])
+    }
 }
